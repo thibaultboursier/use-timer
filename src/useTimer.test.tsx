02a32546b58f7d02bb9a538889ea3e61431e5498
Sepehr Mohammadi <sepehrity@gmail.com>
2020-10-10 00:16:41 +0330

    refactor: rewrite tests with @testing-library/react-hooks library

--- src/useTimer.test.tsx
+++ src/useTimer.test.tsx
@@ -1,604 +1,356 @@
-import 'jsdom-global/register';
-import React from 'react';
 import { useTimer } from './useTimer';
-import { act } from 'react-dom/test-utils';
-import { render, fireEvent } from '@testing-library/react';
+import { renderHook, act } from '@testing-library/react-hooks';
 
 jest.useFakeTimers();
 
 describe('Start', () => {
   it('should start timer', () => {
     // Given
-    const Component = () => {
-      const { time, start } = useTimer();
-
-      return (
-        <div>
-          <button onClick={start}>Start</button>
-          <p data-testid="time">{time}</p>
-        </div>
-      );
-    };
-
-    const { getByRole, getByTestId } = render(<Component />);
+    const { result } = renderHook(() => useTimer());
 
     // When
-    fireEvent.click(getByRole('button'));
-
     act(() => {
+      result.current.start();
       jest.advanceTimersByTime(5000);
     });
 
     // Then
-    expect(getByTestId('time').textContent).toBe('5');
+    expect(result.current.time).toBe(5);
   });
 
   it('should start timer with an initial time of 10', () => {
     // Given
-    const Component = () => {
-      const { time, start } = useTimer({
-        initialTime: 10,
-      });
-
-      return (
-        <div>
-          <button onClick={start}>Start</button>
-          <p data-testid="time">{time}</p>
-        </div>
-      );
-    };
-
-    const { getByRole, getByTestId } = render(<Component />);
+    const { result } = renderHook(() => useTimer({ initialTime: 10 }));
 
     // When
-    fireEvent.click(getByRole('button'));
-
     act(() => {
+      result.current.start();
       jest.advanceTimersByTime(5000);
     });
 
     // Then
-    expect(getByTestId('time').textContent).toBe('15');
+    expect(result.current.time).toBe(15);
   });
 
   it('should start decremental timer with an initial time of 100', () => {
     // Given
-    const Component = () => {
-      const { time, start } = useTimer({
+    const { result } = renderHook(() =>
+      useTimer({
         initialTime: 100,
         timerType: 'DECREMENTAL',
-      });
-
-      return (
-        <div>
-          <button onClick={start}>Start</button>
-          <p data-testid="time">{time}</p>
-        </div>
-      );
-    };
-
-    const { getByRole, getByTestId } = render(<Component />);
+      })
+    );
 
     // When
-    fireEvent.click(getByRole('button'));
-
     act(() => {
+      result.current.start();
       jest.advanceTimersByTime(20000);
     });
 
     // Then
-    expect(getByTestId('time').textContent).toBe('80');
+    expect(result.current.time).toBe(80);
   });
 
   it('should update time with an interval of 2000 milliseconds', () => {
     // Given
-    const Component = () => {
-      const { time, start } = useTimer({
+    const { result } = renderHook(() =>
+      useTimer({
         interval: 2000,
-      });
-
-      return (
-        <div>
-          <button onClick={start}>Start</button>
-          <p data-testid="time">{time}</p>
-        </div>
-      );
-    };
-
-    const { getByRole, getByTestId } = render(<Component />);
+      })
+    );
 
     // When
-    fireEvent.click(getByRole('button'));
-
     act(() => {
+      result.current.start();
       jest.advanceTimersByTime(10000);
     });
 
     // Then
-    expect(getByTestId('time').textContent).toBe('5');
+    expect(result.current.time).toBe(5);
   });
 });
 
 describe('Stop', () => {
   it('should stop incremental timer when time is over', () => {
     // Given
-    const Component = () => {
-      const { time, start } = useTimer({
+    const { result } = renderHook(() =>
+      useTimer({
         endTime: 25,
         initialTime: 5,
-      });
-
-      return (
-        <div>
-          <button onClick={start}>Start</button>
-          <p data-testid="time">{time}</p>
-        </div>
-      );
-    };
-
-    const { getByRole, getByTestId } = render(<Component />);
+      })
+    );
 
     // When
-    fireEvent.click(getByRole('button'));
 
     act(() => {
+      result.current.start();
       jest.advanceTimersByTime(40000);
     });
 
     // Then
-    expect(getByTestId('time').textContent).toBe('25');
+    expect(result.current.time).toBe(25);
   });
 
   it('should stop decremental timer when time is over', () => {
     // Given
-    const Component = () => {
-      const { time, start } = useTimer({
+    const { result } = renderHook(() =>
+      useTimer({
         endTime: 10,
         initialTime: 30,
         timerType: 'DECREMENTAL',
-      });
-
-      return (
-        <div>
-          <button onClick={start}>Start</button>
-          <p data-testid="time">{time}</p>
-        </div>
-      );
-    };
-
-    const { getByRole, getByTestId } = render(<Component />);
+      })
+    );
 
     // When
-    fireEvent.click(getByRole('button'));
-
     act(() => {
+      result.current.start();
       jest.advanceTimersByTime(30000);
     });
 
     // Then
-    expect(getByTestId('time').textContent).toBe('10');
+    expect(result.current.time).toBe(10);
   });
 });
 
 describe('Pause', () => {
   it('should pause timer', () => {
     // Given
-    const Component = () => {
-      const { time, start, pause } = useTimer();
-
-      return (
-        <div>
-          <button data-testid="start" onClick={start}>
-            Start
-          </button>
-          <button data-testid="pause" onClick={pause}>
-            Start
-          </button>
-          <p data-testid="time">{time}</p>
-        </div>
-      );
-    };
-
-    const { getByTestId } = render(<Component />);
+    const { result } = renderHook(() => useTimer());
 
     // When
-    fireEvent.click(getByTestId('start'));
-
     act(() => {
+      result.current.start();
       jest.advanceTimersByTime(5000);
-    });
-
-    fireEvent.click(getByTestId('pause'));
-
-    act(() => {
+      result.current.pause();
       jest.advanceTimersByTime(5000);
     });
 
     // Then
-    expect(getByTestId('time').textContent).toBe('5');
+    expect(result.current.time).toBe(5);
   });
 
   it('should pause timer with an end time', () => {
     // Given
-    const Component = () => {
-      const { time, start, pause } = useTimer({
+    const { result } = renderHook(() =>
+      useTimer({
         endTime: 5,
-      });
-
-      return (
-        <div>
-          <button data-testid="start" onClick={start}>
-            Start
-          </button>
-          <button data-testid="pause" onClick={pause}>
-            Start
-          </button>
-          <p data-testid="time">{time}</p>
-        </div>
-      );
-    };
-
-    const { getByTestId } = render(<Component />);
-    const startButton = getByTestId('start');
-    const pauseButton = getByTestId('pause');
-    const time = getByTestId('time');
+      })
+    );
 
     // When
-    fireEvent.click(startButton);
-
     act(() => {
+      result.current.start();
       jest.advanceTimersByTime(3000);
-    });
-
-    fireEvent.click(pauseButton);
-
-    act(() => {
+      result.current.pause();
       jest.advanceTimersByTime(5000);
+      result.current.start();
     });
-
-    fireEvent.click(startButton);
 
     // Then
-    expect(time.textContent).toBe('3');
+    expect(result.current.time).toBe(3);
 
     act(() => {
       jest.advanceTimersByTime(3000);
     });
 
-    expect(time.textContent).toBe('5');
+    expect(result.current.time).toBe(5);
   });
 });
 
 describe('Reset', () => {
   it('should reset timer to default initial time', () => {
     // Given
-    const Component = () => {
-      const { time, start, reset } = useTimer();
-
-      return (
-        <div>
-          <button data-testid="start" onClick={start}>
-            Start
-          </button>
-          <button data-testid="reset" onClick={reset}>
-            Reset
-          </button>
-          <p data-testid="time">{time}</p>
-        </div>
-      );
-    };
-
-    const { getByTestId } = render(<Component />);
-    const startButton = getByTestId('start');
-    const resetButton = getByTestId('reset');
-    const time = getByTestId('time');
+    const { result } = renderHook(() => useTimer());
 
     // When
-    fireEvent.click(startButton);
-
     act(() => {
+      result.current.start();
       jest.advanceTimersByTime(5000);
+      result.current.reset();
     });
 
-    fireEvent.click(resetButton);
-
     // Then
-    expect(time.textContent).toBe('0');
+    expect(result.current.time).toBe(0);
   });
 
   it('should reset timer to default initial time after restart', () => {
     // Given
-    const Component = () => {
-      const { time, start } = useTimer({
+    const { result } = renderHook(() =>
+      useTimer({
         endTime: 10,
-      });
-
-      return (
-        <div>
-          <button onClick={start}>Start</button>
-          <p data-testid="time">{time}</p>
-        </div>
-      );
-    };
-
-    const { getByRole, getByTestId } = render(<Component />);
+      })
+    );
 
     // When
-    fireEvent.click(getByRole('button'));
-
     act(() => {
+      result.current.start();
       jest.advanceTimersByTime(10000);
-    });
-
-    fireEvent.click(getByRole('button'));
-
-    act(() => {
+      result.current.start();
       jest.advanceTimersByTime(5000);
     });
 
     // Then
-    expect(getByTestId('time').textContent).toBe('5');
+    expect(result.current.time).toBe(5);
   });
 
   it('should reset timer to initial time of 20', () => {
     // Given
-    const Component = () => {
-      const { time, start, reset } = useTimer({
+    const { result } = renderHook(() =>
+      useTimer({
         initialTime: 20,
-      });
-
-      return (
-        <div>
-          <button data-testid="start" onClick={start}>
-            Start
-          </button>
-          <button data-testid="reset" onClick={reset}>
-            Reset
-          </button>
-          <p data-testid="time">{time}</p>
-        </div>
-      );
-    };
-
-    const { getByTestId } = render(<Component />);
+      })
+    );
 
     // When
-    fireEvent.click(getByTestId('start'));
-
     act(() => {
+      result.current.start();
+      jest.advanceTimersByTime(5000);
+      result.current.reset();
       jest.advanceTimersByTime(5000);
     });
 
-    fireEvent.click(getByTestId('reset'));
-
     // Then
-    expect(getByTestId('time').textContent).toBe('20');
+    expect(result.current.time).toBe(20);
   });
 });
 
 describe('Advance time', () => {
   it('should advance time and add 10 to time value', () => {
     // Given
-    const Component = () => {
-      const { advanceTime, time, start } = useTimer({
+    const { result } = renderHook(() =>
+      useTimer({
         initialTime: 0,
-      });
-
-      return (
-        <div>
-          <button data-testid="start" onClick={start}>
-            Start
-          </button>
-          <button data-testid="advanceTime" onClick={() => advanceTime(10)}>
-            Advance time
-          </button>
-          <p data-testid="time">{time}</p>
-        </div>
-      );
-    };
-
-    const { getByTestId } = render(<Component />);
+      })
+    );
 
     // When
-    fireEvent.click(getByTestId('start'));
-
     act(() => {
+      result.current.start();
       jest.advanceTimersByTime(5000);
+      result.current.advanceTime(10);
     });
 
-    // When
-    fireEvent.click(getByTestId('advanceTime'));
-
     // Then
-    expect(getByTestId('time').textContent).toBe('15');
+    expect(result.current.time).toBe(15);
   });
 
   it('should advance time and remove 5 to time value when timer type is decremental', () => {
     // Given
-    const Component = () => {
-      const { advanceTime, time, start } = useTimer({
+    const { result } = renderHook(() =>
+      useTimer({
         initialTime: 30,
         timerType: 'DECREMENTAL',
-      });
-
-      return (
-        <div>
-          <button data-testid="start" onClick={start}>
-            Start
-          </button>
-          <button data-testid="advanceTime" onClick={() => advanceTime(5)}>
-            Advance time
-          </button>
-          <p data-testid="time">{time}</p>
-        </div>
-      );
-    };
-
-    const { getByTestId } = render(<Component />);
+      })
+    );
 
     // When
-    fireEvent.click(getByTestId('start'));
-
     act(() => {
+      result.current.start();
       jest.advanceTimersByTime(10000);
+      result.current.advanceTime(5);
     });
 
-    // When
-    fireEvent.click(getByTestId('advanceTime'));
-
     // Then
-    expect(getByTestId('time').textContent).toBe('15');
+    expect(result.current.time).toBe(15);
   });
 
   it('should continue to update time after it has been advanced', () => {
     // Given
-    const Component = () => {
-      const { advanceTime, time, start } = useTimer({
+    const { result } = renderHook(() =>
+      useTimer({
         initialTime: 0,
-      });
-
-      return (
-        <div>
-          <button data-testid="start" onClick={start}>
-            Start
-          </button>
-          <button data-testid="advanceTime" onClick={() => advanceTime(50)}>
-            Advance time
-          </button>
-          <p data-testid="time">{time}</p>
-        </div>
-      );
-    };
-
-    const { getByTestId } = render(<Component />);
+      })
+    );
 
     // When
-    fireEvent.click(getByTestId('start'));
-
     act(() => {
+      result.current.start();
       jest.advanceTimersByTime(10000);
-    });
-
-    // When
-    fireEvent.click(getByTestId('advanceTime'));
-
-    act(() => {
+      result.current.advanceTime(50);
       jest.advanceTimersByTime(20000);
     });
 
     // Then
-    expect(getByTestId('time').textContent).toBe('80');
+    expect(result.current.time).toBe(80);
   });
 });
 
 describe('State and callbacks', () => {
   it('should display "Running" text when timer is running', () => {
     // Given
-    const Component = () => {
-      const { isRunning, start, pause, reset } = useTimer({
+    const { result } = renderHook(() =>
+      useTimer({
         initialTime: 20,
-      });
-
-      return (
-        <div>
-          <button data-testid="start" onClick={start}>
-            Start
-          </button>
-          <button data-testid="pause" onClick={pause}>
-            Pause
-          </button>
-          <button data-testid="reset" onClick={reset}>
-            Reset
-          </button>
-          <p data-testid="status">{isRunning ? 'Running' : 'Not running'}</p>
-        </div>
-      );
-    };
-
-    const { getByTestId } = render(<Component />);
-    const startButton = getByTestId('start');
-    const pauseButton = getByTestId('pause');
-    const resetButton = getByTestId('reset');
-    const statusBlock = getByTestId('status');
+      })
+    );
 
     // When
-    fireEvent.click(startButton);
+    act(() => {
+      result.current.start();
+    });
 
     // Then
-    expect(statusBlock.textContent).toBe('Running');
+    expect(result.current.isRunning).toBe(true);
 
     // When
-    fireEvent.click(pauseButton);
+    act(() => {
+      result.current.pause();
+    });
 
     // Then
-    expect(statusBlock.textContent).toBe('Not running');
+    expect(result.current.isRunning).toBe(false);
 
     // When
-    fireEvent.click(startButton);
+    act(() => {
+      result.current.start();
+    });
 
     // Then
-    expect(statusBlock.textContent).toBe('Running');
+    expect(result.current.isRunning).toBe(true);
 
     // When
-    fireEvent.click(resetButton);
+    act(() => {
+      result.current.reset();
+    });
 
     // Then
-    expect(statusBlock.textContent).toBe('Not running');
+    expect(result.current.isRunning).toBe(false);
   });
 
   it('should call callback function when time is over', () => {
     // Given
     const onTimeOver = jest.fn();
-    const Component = () => {
-      const { start } = useTimer({
+    const { result } = renderHook(() =>
+      useTimer({
         endTime: 30,
         initialTime: 0,
         onTimeOver,
-      });
-
-      return (
-        <div>
-          <button onClick={start}>Start</button>
-        </div>
-      );
-    };
-
-    const { getByRole } = render(<Component />);
+      })
+    );
 
     // When
-    fireEvent.click(getByRole('button'));
-
     act(() => {
+      result.current.start();
       jest.advanceTimersByTime(30000);
     });
 
     // Then
-    expect(onTimeOver).toHaveBeenCalled();
+    expect(onTimeOver).toHaveBeenCalledTimes(1);
   });
 
   it('should call callback function when time is updated', () => {
     // Given
     const onTimeUpdate = jest.fn();
-    const Component = () => {
-      const { start } = useTimer({
-        endTime: 10,
+    const { result } = renderHook(() =>
+      useTimer({
+        endTime: 30,
         initialTime: 0,
         onTimeUpdate,
-      });
-
-      return (
-        <div>
-          <button onClick={start}>Start</button>
-        </div>
-      );
-    };
-
-    const { getByRole } = render(<Component />);
-
+      })
+    );
     // When
-    fireEvent.click(getByRole('button'));
-
     act(() => {
+      result.current.start();
       jest.advanceTimersByTime(10000);
     });
 

8b51b0b5f7b05df9eb3861b4b5513d660de5c2d5
Thibault <boursier.thibault@gmail.com>
2020-10-23 00:00:43 +0200

    Add example folder with automatic deploy (#52)
    * feat: add demo with parcel
    
    * chore: exclude demo folder from npm package
    
    * chore: add custom demo build folder
    
    * chore: update demo scripts

---
+++ demo/App/components/RunningButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RunningButton = () => (
+  <button className="btn btn-primary" disabled style={{ cursor: 'default' }}>
+    Running...
+  </button>
+);
+
+export default RunningButton;
